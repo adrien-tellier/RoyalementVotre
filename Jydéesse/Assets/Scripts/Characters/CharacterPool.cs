51f92c67c32ce20f774ef72d2207b61bd33f25cd
Adrien Tellier <a.tellier@student.isartdigital.com>
2020-09-08 14:44:12 +0200

    Player movement + CharacterPool creation + Character creation

---
+++ Jydéesse/Assets/Scripts/Characters/CharacterPool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterPool : MonoBehaviour
+{
+    [SerializeField]
+    private Player m_player = null;
+
+    private Character[] m_characters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_characters = gameObject.GetComponentsInChildren<Character>();
+        Debug.Log(m_characters.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SendPositionToPlayer(Vector3 position)
+    {
+        Debug.Log(position);
+        m_player.Destination = position;
+    }
+}
---
+++ Jydéesse/Assets/Scripts/Characters/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float m_speed = 1f;
+
+    [SerializeField]
+    private float m_stopDistance = 5f;
+
+    private Vector3 m_destination;
+
+    public Vector3 Destination { get { return m_destination; } set { m_destination = value; } }
+
+    private void Awake()
+    {
+        m_destination = transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position, m_destination) >= m_stopDistance)
+            transform.position += (m_destination - transform.position).normalized * m_speed * Time.fixedDeltaTime;
+    }
+}

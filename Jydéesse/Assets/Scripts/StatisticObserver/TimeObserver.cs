0819ca9c642dda43aa71e1db06a7d3ca92188ec2
Alexis GABORIT <a.gaborit@student.isartdigital.com>
2020-09-10 11:49:14 +0200

    added time display

---
+++ Jydéesse/Assets/Scripts/StatisticObserver/TimeObserver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeObserver : MonoBehaviour
+{
+    [SerializeField]
+    private Player m_player = null;
+
+    [SerializeField]
+    private Text m_infoText = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateDisplay();
+        m_player.e_timeChanged.AddListener(UpdateDisplay);
+    }
+
+    // Update is called once per frame
+    void UpdateDisplay()
+    {
+        m_infoText.text = "Time : \n"+m_player.getTime();
+    }
+}

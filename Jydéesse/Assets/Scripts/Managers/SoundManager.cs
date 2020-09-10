aa3d4c3ef14391eda8695c0dd4bf9ded1edccfe3
Adrien Tellier <a.tellier@student.isartdigital.com>
2020-09-10 15:26:38 +0200

    Sound System SetUp

---
+++ Jydéesse/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer m_mixer = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		LoadVolumes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	}
+
+	private void OnDestroy()
+	{
+		SaveVolumes();
+	}
+
+	public float GetMasterVolume()
+	{
+		m_mixer.GetFloat("MasterVolume", out float volume);
+		return volume;
+	}
+
+	public float GetMusicVolume()
+	{
+		m_mixer.GetFloat("MusicVolume", out float volume);
+		return volume;
+	}
+
+	public float GetSFXVolume()
+	{
+		m_mixer.GetFloat("SFXVolume", out float volume);
+		return volume;
+	}
+
+	public void SetMasterVolume(float volume)
+	{
+		if (volume <= -20)
+			volume = -80;
+
+		m_mixer.SetFloat("MasterVolume", volume);
+	}
+
+	public void SetMusicVolume(float volume)
+	{
+		if (volume <= -20)
+			volume = -80;
+
+		m_mixer.SetFloat("MusicVolume", volume);
+	}
+
+	public void SetSFXVolume(float volume)
+	{
+		if (volume <= -20)
+			volume = -80;
+
+		m_mixer.SetFloat("SFXVolume", volume);
+	}
+
+	public void SaveVolumes()
+	{
+		PlayerPrefs.SetFloat("MasterVolume", GetMasterVolume());
+		PlayerPrefs.SetFloat("MusicVolume", GetMusicVolume());
+		PlayerPrefs.SetFloat("SFXVolume", GetSFXVolume());
+	}
+
+	public void LoadVolumes()
+	{
+		SetMasterVolume(PlayerPrefs.GetFloat("MasterVolume"));
+		SetMusicVolume(PlayerPrefs.GetFloat("MusicVolume"));
+		SetSFXVolume(PlayerPrefs.GetFloat("SFXVolume"));
+	}
+}
